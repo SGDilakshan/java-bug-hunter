ace7b15446f329b3c18a520c9b0b51192a9d0059
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-09 13:18:40 +0530

    upload the faster runner answer

---
+++ src/Week_01/Q3_FasterRunner.java
@@ -0,0 +1,46 @@
+package Week_01;
+
+/*
+3. The Faster Runner
+Two friends, Arjun and Varun, run a race. Print who is faster.
+
+Input Format
+• Two integers time1 and time2 (in seconds)
+
+Output Format
+• "Arjun" if Arjun is faster
+• "Varun" if Varun is faster
+• "Tie" if both are equal
+
+Sample Test Cases
+Input 1:
+12 10
+Output 1:
+Varun
+
+Input 2:
+15 20
+Output 2:
+Arjun
+
+Input 3:
+18 18
+Output 3:
+Tie
+*/
+
+public class Q3_FasterRunner {
+    public static void main(String[] args) {
+        int time1 = 18;
+        int time2 = 18;
+
+        if (time1 < time2) {
+            System.out.println("Arjun"); // Arjun took less time → faster
+        } else if (time2 < time1) {
+            System.out.println("Varun"); // Varun took less time → faster
+        } else {
+            System.out.println("Tie");
+        }
+
+    }
+}
