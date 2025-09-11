f3f72e80079778ad67967b39399a4d8c14c2b76f
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-12 01:46:01 +0530

    Upload the pass or fail in exam answer

---
+++ src/Week_01/Optional/Q2_PassOrFail.java
@@ -0,0 +1,13 @@
+package Week_01.Optional;
+
+public class Q2_PassOrFail {
+    public static void main(String[] args) {
+        int mark = 78;
+
+        if (mark >= 50 && mark <= 100) {
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+    }
+}
