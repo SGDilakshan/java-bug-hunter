4d9088acd40088e045bcb423b9f124d05b931975
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-12 01:47:08 +0530

    Upload the grade calculator answer

---
+++ src/Week_01/Optional/Q4_Calculator.java
@@ -0,0 +1,17 @@
+package Week_01.Optional;
+
+public class Q4_Calculator {
+    public static void main(String[] args) {
+        int mark = 80;
+
+        if (mark >= 90) {
+            System.out.println("Grade A");
+        }else if (mark >= 75) {
+            System.out.println("Grade B");
+        }else if (mark >= 50) {
+            System.out.println("Grade C");
+        }else{
+            System.out.println("Fail");
+        }
+    }
+}
