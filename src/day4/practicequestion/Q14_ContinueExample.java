44e74c3eae3762347e324dc25b2ed83f1227f0d0
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-14 16:44:18 +0530

    continue example answer updated

--- src/day4/practicequestion/Q14_ContinueExample.java
+++ src/day4/practicequestion/Q14_ContinueExample.java
@@ -1,6 +1,6 @@
 package day4.practicequestion;
 
-// Write a Java program to print numbers from 1 to 5, but skip the number 3 using the continue statement.
+// Write a Java program to print numbers from 1 to 5, but skip the number 3 using the continue statement
 
 public class Q14_ContinueExample {
     public static void main(String[] args) {
