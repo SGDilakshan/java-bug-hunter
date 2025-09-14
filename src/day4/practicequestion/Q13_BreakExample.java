e3071b41c64db286616216a1cfe4423e66cda036
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-14 16:43:33 +0530

    break example answer updated

--- src/day4/practicequestion/Q13_BreakExample.java
+++ src/day4/practicequestion/Q13_BreakExample.java
@@ -1,6 +1,6 @@
 package day4.practicequestion;
 
-// Write a Java program to print numbers from 6 down to 1, but stop the loop when the number is 3 using the break statement.
+// Write a Java program to print numbers from 6 down to 1, but stop the loop when the number is 3 using the break statement
 
 public class Q13_BreakExample{
     public static void main(String[] args) {
