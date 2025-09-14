1cbeaa2d84f7cdaace7facc3962a64c27accf14f
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-14 13:09:02 +0530

    Print the first 10 terms of an Arithmetic Progression (AP) starting from 5 with a common difference of 3 in reverse order answer updated

---
+++ src/day4/practicequestion/Q7_ArithmeticProgressionReverse.java
@@ -0,0 +1,18 @@
+package day4.practicequestion;
+
+// Print the first 10 terms of an Arithmetic Progression (AP) starting from 5 with a common difference of 3 in reverse order
+
+public class Q7_ArithmeticProgressionReverse {
+    public static void main(String[] args) {
+        int firstTerm = 5;
+        int difference = 3;
+        int n = 10; // number of terms
+
+        System.out.println("first 10 terms of an AP starting from 5 with a common difference of 3 in reverse order: ");
+
+        for (int i = n; i >= 1; i--) {
+            int term = firstTerm + (i-1) * difference;
+            System.out.print(term + " ");
+        }
+    }
+}
