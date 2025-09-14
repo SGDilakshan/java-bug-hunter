cbb885529b3d07c83a3bba3ca359a942458a8cfe
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-14 14:42:24 +0530

    Write a Java program to print a right-angled triangle of asterisks with 4 rows answer uploaded

---
+++ src/day4/practicequestion/Q9_RightTriangle.java
@@ -0,0 +1,14 @@
+package day4.practicequestion;
+
+// Write a Java program to print a right-angled triangle of asterisks with 4 rows.
+
+public class Q9_RightTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
