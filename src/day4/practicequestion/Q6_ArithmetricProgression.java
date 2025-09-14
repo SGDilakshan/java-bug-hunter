08b7ef89961037c2ce085bbc7fea9d862032bb52
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-14 12:46:12 +0530

    Print the first 10 terms of an Arithmetic Progression (AP) starting from 5 with a common difference of 3 answer updated

---
+++ src/day4/practicequestion/Q6_ArithmetricProgression.java
@@ -0,0 +1,17 @@
+package day4.practicequestion;
+
+// Print the first 10 terms of an Arithmetic Progression (AP) starting from 5 with a common difference of 3.
+
+public class Q6_ArithmetricProgression {
+    public static void main(String[] args) {
+        int firstTerm = 5;
+        int difference = 3;
+
+        System.out.println("First 10 terms of the AP are:");
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.print(firstTerm + " ");
+            firstTerm += difference;
+        }
+    }
+}
