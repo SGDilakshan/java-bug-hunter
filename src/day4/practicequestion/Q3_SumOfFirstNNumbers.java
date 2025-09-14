ee0f99e5a90203322cf748c32b287e3f1107aa47
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-14 11:15:02 +0530

    Write a Java program to find and display the sum of the first n natural numbers answer

---
+++ src/day4/practicequestion/Q3_SumOfFirstNNumbers.java
@@ -0,0 +1,16 @@
+package day4.practicequestion;
+
+// Write a Java program to find and display the sum of the first n natural numbers (assign the value of n in the code)
+
+public class Q3_SumOfFirstNNumbers {
+    public static void main(String[] args) {
+        int n = 9;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+    }
+}
