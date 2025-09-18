2aecdee6270afceef283adb5f37956d39bccff82
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-18 21:17:46 +0530

    Upload a 0-1 Triangle Pattern print method 2 answer

--- src/day5/practicequestion/Q04_ZeroOneTriangle.java
+++ src/day5/practicequestion/Q04_ZeroOneTriangle.java
@@ -4,6 +4,10 @@ package day5.practicequestion;
 
 public class Q04_ZeroOneTriangle {
     public static void main(String[] args) {
+
+        /*
+
+        Method - 1
         for (int i=1; i<=5; i++) {
             for (int j=1; j<=i; j++) {
                 if ((i+j)%2==0) {
@@ -14,5 +18,24 @@ public class Q04_ZeroOneTriangle {
             }
             System.out.println();
         }
+         */
+
+        for (int x = 1; x <= 5; x++) {
+            boolean value = true;
+
+            if (x % 2 == 0) {
+                value = false;
+            }
+            for (int y = 1; y <= x; y++) {
+                if(value) {
+                    System.out.print(1);
+                }else{
+                    System.out.print(0);
+                }
+                value = !value;
+            }
+            System.out.println();
+        }
+
     }
 }
