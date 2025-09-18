e7d3cf690087babf7debc2bca7192d91678f02c2
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-19 01:36:56 +0530

    Print Squares of Numbers by for loop Answer

---
+++ src/week2/assignment/Q06_PrintSquares.java
@@ -0,0 +1,13 @@
+package week2.assignment;
+
+/*
+Print the squares of numbers from 1 to 15 using a for loop.
+*/
+
+public class Q06_PrintSquares {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("Square of " + i + " is " + (i * i));
+        }
+    }
+}
