fbd06e00d382382fd0a563ba07fb673a14bd46a2
SGDilakshan <sgd.dilakshan@gmail.com>
2025-09-20 23:47:22 +0530

    Write a Java program to calculate the factorial of a number using recursion answer uploaded

---
+++ src/day6/practicequestion/Q04_FactorialCalculator.java
@@ -0,0 +1,19 @@
+package day6.practicequestion;
+
+// Write a Java program to calculate the factorial of a number using recursion.
+
+public class Q04_FactorialCalculator {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + result);
+    }
+}
